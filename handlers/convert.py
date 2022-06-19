1b3881c08d28ff793740fd03c9d77c387330c050
Slamaio <konnor.ru92@gmail.com>
2022-06-19 17:15:28 +0300

    tidied up the link conversion

--- handlers/convert.py
+++ handlers/convert.py
@@ -1,20 +1,36 @@
-from aiogram import types
 import requests
+from aiogram import types
+
+from config import HEADERS
 
 
 async def convert(message: types.Message):
+    """Generate a songwhip link from a regular music link"""
+    
+    await message.answer_chat_action("typing")
+    
     url_entity = next((entity for entity in message.entities if entity.type == "url"), None)
     
     if not url_entity:
         await message.reply("Please send a link")
         return
     
-    headers = {"User-agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.80 Safari/537.36"}
     url = message.text[url_entity.offset : url_entity.offset+url_entity.length]
-    request = requests.post("https://songwhip.com/", data=url, headers=headers)
+    request = requests.post("https://songwhip.com/", data=url, headers=HEADERS)
     
     if not request.ok:
-        await message.reply("Broken link")
+        await message.reply("*There's a problem with that URL\.*\n"
+                            + "Either we don't support this music service or the URL is malformed\. "
+                            + "Try one from a different music service\.",
+                            parse_mode="MarkdownV2")
         return
     
-    await message.reply(request.json()["url"], disable_web_page_preview=False)
+    data: dict = request.json()
+    
+    reply_text = f'*{data["name"]} \({data["type"]}\)*\n' \
+                 + (f'by {", ".join([artist["name"] for artist in data["artists"]])}\n\n'
+                    if "artists" in data.keys()
+                    else "\n\n") \
+                 + data["url"]
+                 
+    await message.reply(reply_text, parse_mode="MarkupV2")
